ad0b260023c1266cfddca97d5682166cae8f7d7b
mikihouse1023 <167415128+mikihouse1023@users.noreply.github.com>
2024-11-21 17:51:21 +0900

    Add files via upload

---
+++ storage/framework/views/d235e7a598ca60a87cc720f45757285d.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Players</title>
+</head>
+<body>
+    <p><?php echo e($params['test']); ?></p>
+    <p><?php echo e($params['sample']); ?></p>
+</body>
+</html>
+<?php /**PATH /var/laravel-curriculum/curriculum-app/resources/views/index.blade.php ENDPATH**/ ?>
