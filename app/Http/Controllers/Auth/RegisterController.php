16a7ebcbb749c075050f83d4b40b361d54844c37
mikihouse1023 <167415128+mikihouse1023@users.noreply.github.com>
2024-12-15 20:22:10 +0900

    Add files via upload

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+//DBに接続。[国]のドロップダウンメニュー作成に必要
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class RegisterController extends Controller
+{
+     //登録ページ
+     public function showRegistrationForm()
+     {
+         return view('auth.register');
+     }
+//DBに接続。[国]のドロップダウンメニュー作成に必要
+     public function create()
+     {
+         $countries = DB::table('countries')->get();
+         return view('auth.register', compact('countries'));
+     }
+
+
+     public function store(Request $request)
+{    $request->validate([
+    'email' => 'required|email|unique:users,email',
+    'password' => 'required|confirmed',
+]);
+
+$role = $request->user_type === 'admin' ? 0 : 1;
+$country_id = $request->user_type === 'admin' ? 0 : $request->country_id;
+
+// ユーザー名が必要であればフォームに追加し、ここで取得します
+
+
+User::create([
+    'name' => '', // 空文字を設定
+    'email' => $request->email,
+    'password' => Hash::make($request->password),
+    'country_id' => $country_id,
+    'role' => $role,
+]);
+
+return redirect()->route('login');
+ 
+}
+}
